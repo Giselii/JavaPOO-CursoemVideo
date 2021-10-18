772fe757f2919f59432bd674ae6da5a4d214caf6
Giseli Almeida <giselinnes@hotmail.com>
2021-10-18 10:28:36 -0300

    Update Aula06.java

--- aula06/Aula06.java
+++ aula06/Aula06.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aula06;
 
 /**
@@ -10,10 +6,6 @@ package aula06;
  * @author julim
  */
 public class Aula06 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         ControleRemoto c = new ControleRemoto();
         //c.ligar();
