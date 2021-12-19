048103d1bc2aa6fca72654b300e5b3d8cb787713
Giseli Almeida <giselinnes@hotmail.com>
2021-12-19 08:19:37 -0300

    Pessoas_Heranca

---
+++ projetopessoas/src/projetopessoas/Professor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetopessoas;
+
+/**
+ *
+ * @author julim
+ */
+public class Professor extends Pessoa {
+    private String especialidade;
+    private float salario;
+    
+    public void receberAumento(float aum){
+        this.salario += aum;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+    
+}
---
+++ projetopessoas/src/projetopessoas/ProjetoPessoas.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetopessoas;
+
+/**
+ *
+ * @author julim
+ */
+public class ProjetoPessoas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Programa Principal
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+        
+        p1.setNome("Pedro");
+        p2.setNome("Maria");
+        p3.setNome("Cláudio");
+        p4.setNome("Fabiana");
+        
+        p1.setSexo("M");
+        p4.setSexo("F");
+        p2.setIdade(18);
+        
+        p2.setCurso("Informática");
+        p3.setSalario(2500.75f);
+        p4.setSetor("Estoque");
+        
+        
+        
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+        
+        
+        
+        
+    }
+    
+}
